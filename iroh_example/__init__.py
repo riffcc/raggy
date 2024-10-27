e12c65a32dc846d8fdb4f8697dd51104c583b575
Benjamin Arntzen <zorlin@gmail.com>
2024-10-27 20:04:48 +0000

    Remove all

--- iroh_example/__init__.py
+++
@@ -1,3 +0,0 @@
-"""
-Example iroh project
-"""
--- iroh_example/main.py
+++
@@ -1,7 +0,0 @@
-import iroh
-
-async def connect_node():
-    """Create and start an iroh node"""
-    node = iroh.IrohNode()
-    await node.start()
-    return node
