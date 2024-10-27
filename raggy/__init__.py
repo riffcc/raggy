0d468a7d954b45bcaec45effb095be46c015dd1a
Benjamin Arntzen <zorlin@gmail.com>
2024-10-27 19:33:17 +0000

    Remove all RaggyPy code for now

--- raggy/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Raggy - An AI exploration system using Iroh and Streamlit"""
-
-__version__ = "0.1.0"
-
-from .events import EventBus, Event
-from .core import Raggy, RaggyConfig
-from .node import RaggyNode
