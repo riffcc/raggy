32e0d086b930c0c3c40508506ef06d3a159858b0
Benjamin Arntzen <zorlin@gmail.com>
2024-10-27 18:17:22 +0000

    Cleanup

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ set -e
 echo 'Please solve the following issues with Pytest output' | tee test.out
 
 # Install package in development mode
-pip install -e .
+pip3 install -e . --break-system-packages
 
 # Run pytest with detailed output and coverage
 pytest -v --cov=raggy --cov-report=term-missing 2>&1 | tee test.out && plandex tell -f test.out --bg
